b1650d92e5d3b8b5fdb8e01468b74301819358c8
Dyego Felipe de Oliveira <dyegooliveira08@gmail.com>
2021-10-01 21:48:47 -0300

    Resolvendo exercício

--- Módulo II/JAVA OO RECODE PRO/Java - Introdução/src/ExemplosMoodle/custoTotalPecas.java
+++ Módulo II/JAVA OO RECODE PRO/Java - Introdução/src/ExemplosMoodle/custoTotalPecas.java
@@ -1,49 +1,56 @@
 package ExemplosMoodle;
+
 import java.util.Scanner;
 
 public class custoTotalPecas {
 
 	public static void main(String[] args) {
+
+		// Atalho a = new Atalho();
+
+		// Scanner s = new Scanner (System.in);
+
 		
-	//Atalho a = new Atalho();
-		
-		Scanner s = new Scanner (System.in);
-		
-		int continuar=0;
-		double precoTotal=0;
-		int i =10;
-		int [] cod = new int [i];
-		int [] qt = new int [i];
-		double [] subTotal = new double [i];
-		double [] precoItem = new double [i];
-		int j = 0;
-		
-		
-		do {
+		int i = 1;
+		int[] cod = new int[i];
+		int[] qt = new int[i];
+		double[] subTotal = new double[i];
+		double[] precoItem = new double[i];
+		int continuar = 0;
+		double precoTotal = 0;
 	
+
+		do {
+			int j = 0;
 			System.out.println("Digite o código do produto: ");
-			cod[j]= s.nextInt();
+			cod[j] = Atalho.lerInt();
 			System.out.println("Digite o preço deste produto (R$): ");
-			precoItem[j]= s.nextDouble();
+			precoItem[j] = Atalho.lerDouble();
 			System.out.println("Digite a quantidade que você deseja deste produto: ");
-			qt[j] = s.nextInt();
-			subTotal[j] = precoItem[j] * qt [j];
-			precoTotal = precoTotal + subTotal [j];
-			i+=1;
-			j+=1;
+			qt[j] = Atalho.lerInt();
+			subTotal[j] = precoItem[j] * qt[j];
+			precoTotal = precoTotal + subTotal[j];
+			j += 1;
+	
 			System.out.println("Deseja continuar comprando? (1) - SIM / (2) - NÂO");
 			continuar = Atalho.lerInt();
-		}while (continuar == 1);
-		
-		for (j=0;j<i; j++) {
-			System.out.println("Produto: "+cod[j]);
-			System.out.println("Preço unitário: "+precoItem[j]);
-			System.out.println("Quantidade: "+qt[j]);
-			System.out.println("Sub Total = "+subTotal[j]);
+			if ((continuar != 1) && (continuar != 2)) {
+				do {
+					System.out.println("Opção inválida!");
+					System.out.println("Deseja continuar comprando? (1) - SIM / (2) - NÂO");
+					continuar = Atalho.lerInt();
+				} while ((continuar != 1) && (continuar != 2));
+			}
+		} while (continuar == 1);
+
+		for (int j = 0; j < qt.length; j++) {
+			System.out.println("Produto: " + cod[j]);
+			System.out.println("Preço unitário: " + precoItem[j]);
+			System.out.println("Quantidade: " + qt[j]);
+			System.out.println("Sub Total = " + subTotal[j]);
 			System.out.println();
 		}
-		System.out.println("Preço TOTAL = "+precoTotal);
-		
+		System.out.println("Preço TOTAL = " + precoTotal);
 
 	}
 
